83da2819a3ac9eff6e2281bd6c7604877196215a
baran7775 <baranferdos89@gmail.com>
2026-09-03 15:21:46 +0330

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,18 @@
+t=('apple,''grape','apple','orange','apple')
+'''print(t[2])
+print(type(t))
+print(t[2;4])
+for i in t;
+print(i)
+for i in range(0,5);
+print(i+1,t[i])
+i=t.index('apple1')
+print(i)
+r=('hello',)
+print(type)(r)
+t1=(1,2)
+t2=(2,1)
+print(t1==t2)'''
+t1=(1,2)
+t2=(200,100)
+print(t2+t1)
